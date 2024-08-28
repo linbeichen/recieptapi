66c05d6ae7b0ce56aaa5eb380b7451e17726f3fb
Beichen Lin <134591199+linbeichen@users.noreply.github.com>
2024-08-28 21:34:09 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from typing import Optional
-
-from fastapi import FastAPI
-
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import requests
+import json
+ 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+ 
+receiptOcrEndpoint = 'https://ocr.asprise.com/api/v1/receipt'
+item_list = []
+ 
+@app.post("/upload/")
+async def create_upload_file(uploaded_file: UploadFile = File(...)):
+    if uploaded_file.filename[-3:] != "jpg" and uploaded_file.filename[-3:] != "peg" and uploaded_file.filename[-3:] != "png":
+        raise HTTPException(status_code=404, detail="File type incorrect")
+    try:
+        file_location = f"data/sample_image" #get location to store uploaded image
+        with open(file_location, "wb+") as file_object:
+            file_object.write(await uploaded_file.read()) #save the image
+ 
+        with open(file_location, "rb") as file_object:
+            r = requests.post(receiptOcrEndpoint, data={
+                'client_id': 'TEST',
+                'recognizer': 'auto',
+                'ref_no': 'ocr_python_123',
+            }, files={"file": file_object})
+           
+            data = json.loads(r.text) #the receipt info as text
+ 
+            if 'receipts' in data and len(data['receipts']) > 0 and 'items' in data['receipts'][0]:
+                items = data['receipts'][0]['items']
+                for item in items:
+                    item_list.append(item['description']) #ge tthe product name
+    except:
+        raise HTTPException(status_code=404, detail="Error with file scanning.")
+    #print(item_list)
+    return {"item_list": item_list}
